7889d0d680bb93c71aa922e975186e0995753303
David Perez <davidjared123@gmail.com>
2025-09-10 01:24:07 -0400

    activity 2 1 version

--- server.js
+++ server.js
@@ -6,13 +6,24 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+app.use(expressLayouts)
 
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.set("layout", "layouts/layout") // layout relativo a la carpeta views
+
+// Rutas primero
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+// Archivos estáticos después
 app.use(static)
 
 /* ***********************
@@ -28,3 +39,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
